c76520ffed0941eb68dcef4ed691f76d0567dc66
Nick Moreton <nickmoreton@me.com>
2022-06-13 21:57:41 +0100

    Initial commit

---
+++ app/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n*x8mrjfk^rs_064fgpre5_pwb(g_xxzw$u*tr6^uyim!lqz^#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
